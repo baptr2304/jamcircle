deff449dd6d32e562b5975d3b9e3f7ba13844e21
TruyenBD <mrtruyenbd1407@gmail.com>
2024-11-06 13:20:42 +0700

    feat: middleware auth, logout

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,12 @@
 import { $get } from '@/api/axios'
 export function fetchUserData() {
-    return { data: { id: 1, name: 'John Doe' } }
+    return {
+        data: {
+            id: 1,
+            name: "darkdy nasty",
+            email: "m@example.com",
+            avatar: "https://avatars.githubusercontent.com/u/78248024?v=4",
+        }
+    }
     return $get('/user')
 }
