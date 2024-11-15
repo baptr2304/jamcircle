928a83a1f725d8a6f5ffbf78908ef20036aae621
baptr2304 <truongbaophuc456@gmail.com>
2024-11-16 01:06:11 +0700

    update playlist UI

--- src/pages/Playlist.vue
+++ src/pages/Playlist.vue
@@ -1,4 +1,5 @@
 <script setup>
+import IconAddPlaylist from "@/components/icons/IconAddPlaylist.vue";
 import IconMusicSolid from "@/components/icons/IconMusicSolid.vue";
 import PlaylistHeader from "@/components/playlist/PlaylistHeader.vue";
 import router from "@/router";
@@ -21,29 +22,46 @@ const fetchPlaylists = async () => {
 onMounted(() => {
   fetchPlaylists();
 });
-// when click playlist item -> redirect to playlist detail page
+
 const handlePlaylistClick = (playlistId) => {
   router.push(`/playlist/${playlistId}`);
+};
+const handleCreatePlaylist = () => {
+  router.push("/playlist/create");
 };
 </script>
 
 <template>
-  <PlaylistHeader />
-  <div v-if="playlistStore.playlists && playlistStore.playlists.length">
+  <div class="relative">
+    <PlaylistHeader />
     <div
-      v-for="playlist in playlistStore.playlists"
-      :key="playlist?.id"
-      @click="handlePlaylistClick(playlist.id)"
+      class="absolute right-8 top-44 flex justify-center items-center px-4 py-2 rounded-full bg-primary hover:bg-ring cursor-pointer font-semibold"
+    >
+      <IconAddPlaylist class="w-6 h-6" />
+      <div @click="handleCreatePlaylist">Create playlist</div>
+    </div>
+
+    <div
+      v-if="playlistStore.playlists && playlistStore.playlists.length"
+      class="flex flex-wrap gap-5 p-5"
     >
       <div
-        class="w-[22.5rem] bg-secondary border h-[5rem] m-5 flex items-center rounded-md cursor-pointer"
+        v-for="playlist in playlistStore.playlists"
+        :key="playlist?.id"
+        @click="handlePlaylistClick(playlist.id)"
       >
-        <div class="w-[5rem] h-[5rem] flex items-center justify-center">
-          <IconMusicSolid class="w-10 h-10" />
+        <div
+          class="w-[22.5rem] bg-secondary border h-[5rem]  flex items-center rounded-md cursor-pointer"
+        >
+          <div
+            class="w-[5rem] h-[5rem] flex items-center justify-center bg-muted-foreground rounded-tl-md rounded-bl-md"
+          >
+            <IconMusicSolid class="w-10 h-10" />
+          </div>
+          <div class="ml-4">{{ playlist.name }}</div>
         </div>
-        <div class="">{{ playlist.name }}</div>
       </div>
     </div>
+    <div v-else-if="!loading && !error">Không có playlist nào</div>
   </div>
-  <div v-else-if="!loading && !error">Không có playlist nào</div>
 </template>
