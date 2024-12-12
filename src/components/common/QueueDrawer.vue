8bb0bb7de216260c70817134ff65a269fe55c828
TruyenBD <mrtruyenbd1407@gmail.com>
2024-12-12 15:39:02 +0700

    feat: search song + play song + add song to queue

--- src/components/common/QueueDrawer.vue
+++ src/components/common/QueueDrawer.vue
@@ -1,14 +1,20 @@
 <script setup>
 import Drawer from '@/components/base/Drawer.vue'
 import SongListItem from '@/components/common/SongListItem.vue'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
 import { useSongStore } from '@/stores/song'
+
 import emitter from '@/utils/eventBus'
 
 const songStore = useSongStore()
 const isVisible = defineModel()
 
-function handlePlaySong(id) {
-  songStore.playSong(id)
+function handlePlaySong(song) {
+  songStore.playSongInQueue(song)
   emitter.emit('play-song')
 }
 </script>
@@ -19,12 +25,29 @@ function handlePlaySong(id) {
       <h1 class="text-2xl font-bold">
         Queue
       </h1>
-      <div class="grid gap-4 mt-4 h-full max-h-[calc(100%-2rem)] overflow-y-auto scrollbar pb-44 lg:pb-24">
-        <SongListItem v-for="song in songStore.playlist" :key="song.id" :song="song" :class="{ 'text-primary': song.id === songStore.currentSong.id }" @handle-click="handlePlaySong(song.id)">
+      <div class="grid gap-4 mt-4 h-full max-h-[calc(100%-2rem)] overflow-y-auto scrollbar pb-44 lg:pb-24 pr-4">
+        <SongListItem v-for="song in songStore.playlist" :key="song.so_thu_tu" :song="song" :class="{ 'text-primary': song.so_thu_tu === songStore.currentSong.so_thu_tu }" @handle-click="handlePlaySong(song)">
           <template #start>
             <div class="w-4">
-              <Icon v-if="song.id === songStore.currentSong.id" name="IconChart" class="w-4 h-4" />
+              <Icon v-if="song.so_thu_tu === songStore.currentSong.so_thu_tu" name="IconChart" class="w-4 h-4" />
               <span v-else>{{ song.so_thu_tu }}</span>
+            </div>
+          </template>
+          <template #action>
+            <div class="w-[20%]">
+              <Popover>
+                <PopoverTrigger>
+                  <Icon
+                    name="IconEllipsis"
+                    class="w-10 h-6  cursor-pointer"
+                  />
+                </PopoverTrigger>
+                <PopoverContent class="w-25">
+                  <button @click="songStore.handleRemoveFromQueue(song)">
+                    Remove from queue
+                  </button>
+                </PopoverContent>
+              </Popover>
             </div>
           </template>
         </SongListItem>
